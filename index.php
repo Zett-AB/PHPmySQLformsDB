90f3b55a9af8947e4929d1cfe7b81541bc4872f9
Alexander <formysite.ru@yandex.ru>
2022-01-25 13:07:15 +0300

    first_step_of_practice

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Практическая работа по взаимодействию между PHP и SQL</title>
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+    <header class="subtitle">
+        <h1>Практическая работа по SQL и PHP</h1>
+        <?php
+             $nickname=" Александр!";
+             $hello="Привет ";
+             $hellow="Продолжаем работу и изучение PHP и SQL.";
+             echo "<h2>".$hello.$nickname."<br>".$hellow."</h2>";
+        ?>
+    </header>
+    
+</body>
+</html>
